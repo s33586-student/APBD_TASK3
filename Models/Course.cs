47e5db1534623683da0453856a124a24c2ec7849
s33586-student <s33586@pjwstk.edu.pl>
2026-03-24 14:41:15 +0100

    Cloned

---
+++ Models/Course.cs
@@ -0,0 +1,18 @@
+namespace APBD_TASK3.Models;
+
+public sealed class Course
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Category { get; set; } = string.Empty;
+
+    public int Ects { get; set; }
+
+    public int LecturerId { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+}
---
+++ Models/Lecturer.cs
@@ -0,0 +1,14 @@
+namespace APBD_TASK3.Models;
+
+public sealed class Lecturer
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Department { get; set; } = string.Empty;
+
+    public DateTime EmploymentDate { get; set; }
+}
